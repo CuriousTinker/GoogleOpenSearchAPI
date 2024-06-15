205ae648f98a652aed3eaf7d78286f080bec290a
Petchi Muthu <ipetchimuthu.pn@gmail.com>
2024-06-15 11:22:15 +0530

    Update to CLI

--- google_search_api/google-opensearch-cli.py
+++ google_search_api/google-opensearch-cli.py
@@ -1,13 +1,12 @@
-# google-opensearch-cli.py
 import argparse
-from .google_search_api import GoogleSearchAPI
+from google_search_api.google_search_api import GoogleSearchAPI
 
 def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description="CLI utility for Google queries.")
     parser.add_argument('query', type=str, help='Search query string')
     parser.add_argument('--num_results', type=int, default=10, help='Number of results to fetch')
-    
+
     # Parse arguments
     args = parser.parse_args()
 
