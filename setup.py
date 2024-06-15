f1fd8ee60b8a029159113a8f8979d72a34d059a6
Petchi Muthu <ipetchimuthu.pn@gmail.com>
2024-06-15 13:09:46 +0530

    Updated setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='google-opensearch-api',
-    version='1.0.11',
+    version='1.0.12',
     packages=find_packages(),
     install_requires=[
         'requests',
